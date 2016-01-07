1a42bdf3361052560620c1b4241857df3cd81026
Tiange Wu <origami7795@gmail.com>
2016-01-07 01:28:50 -0800

    Added somewhat of a webpage

--- wsgi.py
+++ wsgi.py
@@ -4,12 +4,14 @@ import requests
 import json
 
 app = Flask(__name__)
+app.Debug = True
 
 @app.route('/')
 def hello_world():
     data = requests.get('https://api.github.com/repos/origami7/gitpets').json()
+    user_data = requests.get('https://api.github.com/users/origami7').json()
     print data['name']
-    return render_template('index.html')
+    return render_template('index.html', picture=user_data['avatar_url'])
 
 if __name__ == '__main__':
     app.run()
