4602f4246c2dbaed15e049e28c5c3611b14dc51a
Tiange Wu <origami7795@gmail.com>
2016-01-06 23:46:24 -0800

    Added api request to a repo

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    data = requests.get('https://api.github.com/events').json()
-    print data[0]['public']
+    data = requests.get('https://api.github.com/repos/origami7/gitpets').json()
+    print data['name']
     return render_template('index.html')
 
 if __name__ == '__main__':
